17bc7dc7e4fecdf525ffee892ff37c79b928b71b
hayjothi2002 <hayjothis@gmail.com>
2024-11-08 22:20:12 -0800

    js 3 key

---
+++ Javascript/Function.js
@@ -0,0 +1,18 @@
+function cosmo() //without parameter
+{
+    alert("This normal function jothi prakash is the Play boy  ")
+}
+function beta(name,college) //function withh parameter
+{
+    alert("Name is "+name+"College is"+college)
+
+}
+let alpha=()=>
+{
+    document.writeln("Fat arrow function calling ")
+}
+const sum=(value1,value2)=>
+{
+    return value1+value2
+
+}
